13e327630fd025f3d5f4253ee66aaa51f81d2ba4
Asus <Asus@DESKTOP-7RU5LSB>
2024-11-19 19:17:35 +0530

    Beforeencapsulation

---
+++ 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/Svkp/src/Oops/BeforeEncapsulation.java
@@ -0,0 +1,19 @@
+package Oops;
+
+class Human
+{
+	 int age;
+	String name;
+}
+public class BeforeEncapsulation {
+	public static void main(String[] args) {
+		Human obj=new Human();
+		obj.age=25;
+		obj.name="Rohith";
+		obj.age=30;
+		obj.name="leela";
+		System.out.println("Age is "  +obj.age);
+		System.out.println("Name is " +obj.name);
+	}
+}
+
